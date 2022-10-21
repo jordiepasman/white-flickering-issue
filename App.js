161e3d2377f6eb3f70d15898b752a35c430e2765
Jordie Pasman <jordiepasman@msml.nl>
2022-10-21 12:24:34 +0200

    master

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import { View, StatusBar } from 'react-native'
-import { NavigationContainer } from '@react-navigation/native'
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native'
 // import { navigationRef, isReadyRef } from 'Services/NavigationService'
 import { ApplicationStyles, Colors } from './App/Theme'
 import RootStack from './App/Navigators/RootStack'
 
 const App = () => {
+  // Already tried this but not the correct solution because we use one and white and black backgrounds through the app
+  const MyTheme = {
+    ...DefaultTheme,
+    dark: false,
+    colors: { background: 'transparent' }, // black, white, transparent ect ect...
+  }
+
   return (
-    <NavigationContainer>
+    <NavigationContainer
+    // theme={MyTheme}
+    >
       <StatusBar barStyle="light-content" backgroundColor={Colors.black} />
       <View style={ApplicationStyles.rootContainer}>
         <RootStack />
