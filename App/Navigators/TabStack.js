39f66662e99abadce78329655c699b07fc386ce6
Jordie Pasman <jordiepasman@msml.nl>
2022-10-21 11:54:01 +0200

    master

--- App/Navigators/TabStack.js
+++ App/Navigators/TabStack.js
@@ -15,42 +15,31 @@ export default function TabStack(props) {
       screenOptions={({ route }) => ({
         headerShown: false,
         tabBarShowLabel: false,
-        tabBarActiveTintColor: Colors.secondary,
         tabBarInactiveTintColor: Colors.primary,
         tabBarStyle: ApplicationStyles.navigation.tabBar.tabBarStyle,
         tabBarIcon: ({ focused, color, size }) => {
-          let iconName
           let title
           let text
 
           switch (route.name) {
             case 'Dashboard':
-              iconName = 'dashboard-3-line'
               title = 'Dashboard'
-              color = focused ? Colors.secondary : Colors.icon
               text = focused ? Colors.white : Colors.icon
               break
             case 'Home':
-              iconName = 'archive-line'
               title = 'Home'
-              color = focused ? Colors.secondary : Colors.icon
               text = focused ? Colors.white : Colors.icon
               break
             case 'Settings':
-              iconName = 'settings-3-line'
               title = 'Settings'
-              color = focused ? Colors.secondary : Colors.icon
               text = focused ? Colors.white : Colors.icon
               break
             default:
-              iconName = ''
               color = Colors.lightGrey
           }
 
           return (
             <View style={ApplicationStyles.navigation.tabBar.container}>
-              {/* <Icon name={iconName} size={28} color={color} type="remix" /> */}
-
               <Text style={[ApplicationStyles.navigation.tabBar.text, { color: text }]}>
                 {title}
               </Text>
--- App/Theme/ApplicationStyles.js
+++ App/Theme/ApplicationStyles.js
@@ -1,68 +1,9 @@
-import { Platform, StatusBar } from 'react-native'
+import { Platform } from 'react-native'
 import Colors from './Colors'
 
 export default {
   rootContainer: {
     flex: 1,
-  },
-
-  darkBackground: {
-    backgroundColor: Colors.background,
-  },
-
-  whiteBackground: {
-    backgroundColor: Colors.white,
-  },
-
-  defaultPaddingHorizontal: {
-    paddingHorizontal: 20,
-  },
-
-  defaultPaddingVertical: {
-    paddingVertical: 20,
-  },
-
-  defaultPaddingTop: {
-    paddingTop: 40,
-  },
-
-  defaultMarginBottom: {
-    marginBottom: 10,
-  },
-
-  opacidBorder: {
-    borderBottomWidth: 1,
-    borderColor: Colors.opacidBorder,
-  },
-
-  fill: {
-    width: '100%',
-    height: '100%',
-  },
-
-  htmlContainer: {
-    marginTop: 16,
-  },
-
-  flexGrow: {
-    flexGrow: 1,
-  },
-
-  center: {
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  selfCenter: {
-    alignSelf: 'center',
-  },
-
-  fullWidth: {
-    width: '100%',
-  },
-
-  row: {
-    flexDirection: 'row',
   },
 
   // Navigation:
@@ -88,25 +29,8 @@ export default {
       },
     },
 
-    headerNotchStyle: {
-      height: StatusBar.currentHeight,
-      backgroundColor: Colors.background,
-      borderBottomWidth: 0,
-      shadowRadius: 0,
-      shadowOffset: {
-        height: 0,
-      },
-    },
-
     headerBackTitleStyle: {
       color: Colors.white,
-    },
-
-    // Use for background when list bounces on iOS.
-    scrollColor: {
-      backgroundColor: Colors.background,
-      marginTop: -500,
-      paddingTop: 500,
     },
 
     tabBar: {
@@ -157,61 +81,6 @@ export default {
         justifyContent: 'center',
         backgroundColor: Colors.background,
       },
-    },
-  },
-
-  modal: {
-    closeText: {
-      color: Colors.secondary,
-    },
-
-    container: {
-      flex: 1,
-      justifyContent: 'flex-end',
-      shadowColor: Colors.black,
-      shadowOffset: {
-        width: 0,
-        height: 2,
-      },
-      shadowOpacity: 0.25,
-      shadowRadius: 3.84,
-      elevation: 5,
-    },
-
-    header: {
-      alignItems: 'flex-end',
-      marginRight: 20,
-      marginTop: 12,
-    },
-
-    modalBar: {
-      alignSelf: 'center',
-      backgroundColor: Colors.border,
-      borderRadius: 2.5,
-      height: 7,
-      marginTop: 20,
-      width: 60,
-    },
-
-    headerWithTitle: {
-      alignItems: 'center',
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      marginHorizontal: 20,
-      marginBottom: 20,
-      marginTop: 12,
-    },
-
-    item: {
-      paddingHorizontal: 20,
-      backgroundColor: Colors.lightGrey,
-    },
-
-    view: {
-      backgroundColor: Colors.white,
-      paddingBottom: 30,
-      height: Platform.select({ android: '98%', ios: '94%' }),
-      width: '100%',
     },
   },
 }
